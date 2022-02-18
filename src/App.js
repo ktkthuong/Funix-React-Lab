e437a91db5a88975f947b80e854af99112645487
ktkthuong <ktkthuong@gmail.com>
2022-02-18 15:05:55 +0700

    LAB03_1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from '../../cofusions/src/components/MenuComponent';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
       
     </div>
   );
