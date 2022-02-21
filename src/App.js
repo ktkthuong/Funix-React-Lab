498f700582f2d7fae33191589f706f4d295c2070
ktkthuong <ktkthuong@gmail.com>
2022-02-21 15:35:24 +0700

    ASM1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
+import Menu from './StaffList/StaffListComponent';
 import './App.css';
-import { DISHES } from './shared/dishes';
+import { STAFFS } from './shared/staffs';
 import { Component } from 'react';
 import { render } from '@testing-library/react';
 
@@ -10,7 +10,7 @@ class App extends Component {
   constructor (props){
     super(props);
     this.state = {
-      dishes: DISHES
+      staffs: STAFFS 
     };
   }
   render(){
@@ -18,10 +18,10 @@ class App extends Component {
       <div className="App">
         <Navbar dark color="primary">
           <div className='container'>
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
           </div>
         </Navbar>
-        <Menu dishes={this.state.dishes} />
+        <Menu staffs={this.state.staffs} />
         
       </div>
     );
