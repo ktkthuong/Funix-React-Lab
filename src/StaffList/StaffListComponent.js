2e0f50e38656e2928f347aaeaaaae80b98154a2c
ktkthuong <ktkthuong@gmail.com>
2022-02-27 14:20:59 +0700

    LAB4_1

---
+++ src/StaffList/StaffListComponent.js
@@ -0,0 +1,8 @@
+import React, {Component} from "react";
+
+class Menu extends Component {
+
+    constructor(props){
+        super(props);
+    }
+}
---
+++ src/components/MainComponent.js
@@ -0,0 +1,32 @@
+
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+import './App.css';
+import DishDetail from './DishDetailComponent';
+import { DISHES } from './shared/dishes';
+import React, { Component } from 'react';
+import { render } from '@testing-library/react';
+
+class Main extends Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render(){
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className='container'>
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+        
+      </div>
+    );
+  }  
+}
+
+export default App;
