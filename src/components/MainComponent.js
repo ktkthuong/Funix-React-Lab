92ff1ff748d9b77f73ec2f331aeff419dd52317a
ktkthuong <ktkthuong@gmail.com>
2022-03-03 15:21:37 +0700

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,6 +6,9 @@ import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import {Switch, Route, Redirect} from 'react-router-dom';
 
 
@@ -15,14 +18,20 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
-
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
   render(){
-    const HomePage = () => {
+    const HomePage =() => {
       return(
-        <Home />
+        <Home 
+            dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+            promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+            leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
       );
     }
     return (
