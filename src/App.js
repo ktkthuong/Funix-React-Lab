d182e37768cc795f22ecc2f538a97d29e3c3c4bf
ktkthuong <ktkthuong@gmail.com>
2022-03-06 14:46:43 +0700

    ASM2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 
 import Main from './components/MainComponent';
+import StaffList from './StaffListComponent';
+import { STAFFS } from './shared/staffs';
 import './App.css';
 import {BrowserRouter} from 'react-router-dom';
 
 
 
 class App extends Component {
- 
+  constructor(props){
+    super(props);
+    this.state={
+      staffs:STAFFS
+    };
+  }
+
+
+
   render(){
     return (
       <BrowserRouter>
