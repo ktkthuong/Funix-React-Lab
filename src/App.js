94cf35440210d6f9342f9cee678d8e188556424b
ktkthuong <ktkthuong@gmail.com>
2022-03-06 15:14:54 +0700

    updated ASM2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 
 import Main from './components/MainComponent';
-import StaffList from './StaffListComponent';
-import { STAFFS } from './shared/staffs';
+//import StaffList from './StaffListComponent';
+//import { STAFFS } from './shared/staffs';
 import './App.css';
 import {BrowserRouter} from 'react-router-dom';
 
 
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state={
-      staffs:STAFFS
-    };
-  }
+  // constructor(props){
+  //   super(props);
+  //   this.state={
+  //     staffs:STAFFS
+  //   };
+  // }
 
 
 
--- src/StaffList/StaffListComponent.js
+++ src/StaffList/StaffListComponent.js
@@ -1,8 +1,15 @@
 import React, {Component} from "react";
+import { Card, CardImg, CardTitle, Form, Row, Input, Col, Button } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-class Menu extends Component {
+class StaffList extends Component {
 
     constructor(props){
         super(props);
+        this.state={
+            staffs:this.props.staffs,
+            searchName:"",
+        }
+        this.handleSearch=this.handleSearch.bind(this)
     }
 }
