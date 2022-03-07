1d634fecda3f491c4b36a4dead90db04905ebb6d
ktkthuong <ktkthuong@gmail.com>
2022-03-07 23:05:13 +0700

    add Salarycomponent

--- src/components/SalaryComponent.js
+++ src/components/SalaryComponent.js
@@ -3,6 +3,36 @@ import {Breadcrumb, BreadcrumbItem, Card, CardText, CardTitle} from 'reactstrap'
 import {Link}from 'react-router-dom';
 
 
+const Salary=(props) =>{
+    const basicSalary=3000000;
+    const overTimeSalary=200000;
+    const [sortData, setSortData]=useState("idAscending");
+    const handleSort = (e) => {
+        setSortData(e.target.value);
+    }
 
+    if(sortData === "idAscending") {
+        props.staffs.sort((a,b) => {
+            return(
+                a.id - b.id
+            );
+        });
+    }
+    if(sortData === "idDescending") {
+        props.staffs.sort((a,b) => {
+            return(
+                b.id - a.id
+            );
+        });
+    }
+    if(sortData === "salaryAscending") {
+        props.staffs.sort((a,b) => {
+            return(
+                (a.salaryScale * basicSalary + a.overTime * overTimeSalary) -
+                (b.salaryScale * basicSalary + b.overTime * overTimeSalary)
+            );
+        });
+    }
+}
 
 export default Salary;
