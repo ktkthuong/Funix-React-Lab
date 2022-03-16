311c44586011e93acfdd96d9e339d00cebb05458
ktkthuong <ktkthuong@gmail.com>
2022-03-16 23:26:44 +0700

    add Modal

--- src/components/AddStaffComponent.js
+++ src/components/AddStaffComponent.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Button, Input, Label, Modal, Row, Col, Form, FormGroup, FormFeedback } from 'reactstrap';
+import { Button, Input, Label, Modal, Row, Col, Form, FormGroup, FormFeedback, ModalHeader, ModalBody } from 'reactstrap';
 
 class AddStaff extends Component{
     constructor(props){
@@ -14,15 +14,56 @@ class AddStaff extends Component{
             annualLeave: '',
             overTime: '',
             image: '/assets/images/alberto.png',
+            isOpenModal: false
         }
+        this.toggleModal=this.toggleModal.bind(this);
+    }
+
+    toggleModal(){
+        this.setState({isOpenModal:!this.state.isOpenModal});
     }
 
     render(){
         return(
             <React.Fragment>
-                <Button onClick={this.toggleModal} color='primary' className="addButton">
+                <Button onClick={this.toggleModal} color='info' className="addButton">
                     <span className='fa fa-plus'></span>
                 </Button>
+                <Modal isOpen={this.state.isOpenModal} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Thêm nhân viên</ModalHeader>
+                    <ModalBody>
+                        <Form>
+                            <FormGroup>
+                                <Row>
+                                    <Label htmlFor="name" md={4}>Họ và tên</Label>
+                                    <Col md={8}>
+                                        <Input type='text' id='name' name='name' 
+                                        value={this.state.name} />
+                                    </Col>
+                                </Row>
+                            </FormGroup>
+                            <FormGroup>
+                                <Row>
+                                    <Label htmlFor="doB" md={4}>Ngày sinh</Label>
+                                    <Col md={8}>
+                                        <Input type='date' id='doB' name='doB' 
+                                        value={this.state.doB} />
+                                    </Col>
+                                </Row>
+                            </FormGroup>
+                            <FormGroup>
+                                <Row>
+                                    <Label htmlFor="startDate" md={4}>Ngày vào công ty</Label>
+                                    <Col md={8}>
+                                        <Input type='date' id='startDate' name='startDate' 
+                                        value={this.state.startDate} />
+                                    </Col>
+                                </Row>
+                            </FormGroup>
+                        </Form>
+                    </ModalBody>
+
+                </Modal>
             </React.Fragment>
         )
 
