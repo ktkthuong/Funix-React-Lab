496b7890a56c5561102f5113391a8db29ac8aecc
ktkthuong <ktkthuong@gmail.com>
2022-03-22 17:13:22 +0700

    Fetch Post Comment

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -25,7 +25,7 @@ import {baseUrl} from '../shared/baseUrl';
     }
         
     
-    function RenderComments({comments, addComment, dishId}){
+    function RenderComments({comments, postComment, dishId}){
         if(comments!=null){
             return(
                 <div className='col-12 col-md-5 m-1'>
@@ -44,7 +44,7 @@ import {baseUrl} from '../shared/baseUrl';
                             );
                         })}
                     </ul>
-                    <CommentForm dishId={dishId} addComment={addComment}/>
+                    <CommentForm dishId={dishId} postComment={postComment}/>
                 </div>
             )
         }
@@ -82,7 +82,7 @@ import {baseUrl} from '../shared/baseUrl';
     
         handleSubmit(values){
             this.toggleModal();
-            this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+            this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
         }
         render(){
             return(
@@ -181,7 +181,7 @@ import {baseUrl} from '../shared/baseUrl';
                                 <div className='row'>
                                     <RenderDish dish={props.dish} />
                                     <RenderComments comments={props.comments} 
-                                        addComment={props.addComment} 
+                                        postComment={props.postComment} 
                                         dishId={props.dish.id}/> 
                                 </div>
 
