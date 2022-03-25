c17777fec9a4c8a492d32bef43ebf90c1aa2417f
ktkthuong <ktkthuong@gmail.com>
2022-03-25 22:33:07 +0700

    add again ActionType for reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,5 +1,5 @@
 import { DEPARTMENTS, STAFFS} from '../shared/staffs';
-//import * as ActionTypes from './ActionTypes';
+import * as ActionTypes from './ActionTypes';
 
 
 
@@ -11,11 +11,12 @@ export const initialState ={
 };
 
 export const Reducer = (state = initialState, action) => {
-    // switch (action.type) {
-    //     case ActionTypes.ADD_NEW_STAFF:
-    //         return {...state, staffs: [...state.staffs, action.payload] }
+    switch (action.type) {
+        case ActionTypes.ADD_NEW_STAFF:
+            return {...state, staffs: [...state.staffs, action.payload] }
 
-    // default:
+     default:
             return state;
+    }        
     
 } 
